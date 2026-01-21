7af24731efb54e27dc71afac179ff42adf9c4d91
HomamZituni <homam93@msn.com>
2026-01-20 22:50:34 -0500

    added routers and login functionality

---
+++ routes/users.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+
+router.post('/register', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: 'Email already exists' });
+    
+    const user = new User({ username, email, password });
+    await user.save(); 
+    
+    const { password: _, ...safeUser } = user.toObject();
+    res.status(201).json(safeUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    
+    const user = await User.findOne({ email });
+    if (!user || !(await user.isCorrectPassword(password))) {
+      return res.status(400).json({ message: 'Incorrect email or password' });
+    }
+    
+    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const { password: _, ...safeUser } = user.toObject();
+    
+    res.json({ token, user: safeUser });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+module.exports = router;
